4babb79bcfdfd11f4005af80d5835d7fdf24d1d1
sathya11042000 <sathyastudy2000@gmail.com>
2023-05-17 11:42:28 +0530

    anu done her work

--- src/org/data/Sample.java
+++ src/org/data/Sample.java
@@ -4,5 +4,8 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("java");
 		System.out.println("selenium");
+		
+		System.out.println("framework");
+		System.out.println("cucumber");
 	}
 }
